c5aa54c29314543ca9459fe4e215bd5f0e5823e0
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-08-13 17:58:01 +0300

    FoodsWidget

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/common-section/CommonSection';
+
+import { Container, Row, Col } from 'reactstrap';
+
+import products from '../assets.fake-data/products';
+import ProductCard from '../components/UI/product-card/ProductCard';
+
+import '../styles/all-foods.css';
 
 const AllFoods = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [searchTerm, setSearchTerm] = useState('');
 
-export default AllFoods
+  return (
+    <Helmet title="All-Foods">
+      <CommonSection title="All Foods" />;
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="search__widget d-flex align-items-center justify-content-between w-50">
+                <input type="text" placeholder="I'm looking for...." />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+            {products.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb=4">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default AllFoods;
