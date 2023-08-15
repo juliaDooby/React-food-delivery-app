9c739bc43913f1cb46b910986f5a948e1acbce95
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-08-15 20:06:18 +0300

    tabFormStyle

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -43,10 +43,62 @@ const FoodDetails = () => {
                 <p className="product__price">
                   Price: <span>$34</span>
                 </p>
-                <p>
+                <p className="category mb-5">
                   Category: <span>Burger</span>
                 </p>
                 <button className="addTOCart__btn">Add to Cart</button>
+              </div>
+            </Col>
+            <Col lg="12">
+              <div className="tabs d-flex align-items-center gap-5 py-3">
+                <h6 clasName="tab__active">Description</h6>
+                <h6>Review</h6>
+              </div>
+              <div className="tab__content">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, deleniti
+                  impedit quo exercitationem, dolores quibusdam reiciendis molestiae provident qui
+                  ducimus ab consequuntur et assumenda tenetur quis tempore eos iusto voluptas
+                  sapiente, eligendi iste! Nemo, commodi illum reprehenderit quibusdam laudantium
+                  odit!
+                </p>
+              </div>
+
+              <div className="tab__form mb-3">
+                <div className="review">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">Great product</p>
+                </div>
+
+                <div className="review">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">Great product</p>
+                </div>
+
+                <div className="review">
+                  <p className="user__name mb-0">Jhon Doe</p>
+                  <p className="user__email">jhon1@gmail.com</p>
+                  <p className="feedback__text">Great product</p>
+                </div>
+                <form className="form">
+                  <div className="form__group">
+                    <input type="text" placeholder="Enter your name"></input>
+                  </div>
+
+                  <div className="form__group">
+                    <input type="text" placeholder="Enter your name"></input>
+                  </div>
+
+                  <div className="form__group">
+                    <textarea rows={5} type="text" placeholder="Enter your name"></textarea>
+                  </div>
+
+                  <button type="submit" className="addTOCart__btn">
+                    Submit
+                  </button>
+                </form>
               </div>
             </Col>
           </Row>
