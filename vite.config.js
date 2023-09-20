10df47a2182a9571a55e2b9d5de0a1991b854225
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-09-20 18:57:19 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-food-delivery-app/',
   server: {
     open: true,
   },
   build: {
-    outDir: "build",
+    outDir: 'build',
     sourcemap: true,
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests',
     mockReset: true,
   },
-})
+});
